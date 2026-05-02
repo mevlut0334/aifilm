9d15f4726a59b228ba93e3c00661e53f3dc71c95
MEVLUT TUNCER <mevluttuncer0334@gmail.com>
2026-05-02 15:59:14 +0300

    brightness update

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -67,7 +67,7 @@
             top: 0; left: 0;
             width: 100%; height: 100%;
             object-fit: cover;
-            filter: brightness(0.5);
+            filter: brightness(0.8);
         }
 
         .slide-content {
