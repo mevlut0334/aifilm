d69aaebe28d32eab6f7ece95818a7c6d476ae12d
MEVLUT TUNCER <mevluttuncer0334@gmail.com>
2026-05-03 12:38:46 +0300

    sliderresource update

--- app/Http/Resources/Api/V1/SliderResource.php
+++ app/Http/Resources/Api/V1/SliderResource.php
@@ -15,7 +15,7 @@ class SliderResource extends JsonResource
             'description' => $this->description,
             'button_text' => $this->button_text,
             'button_link' => $this->button_link,
-            'image_url'   => $this->image_url,
+            'image_url'   => $this->image_url ? asset($this->image_url) : null,
             'order'       => $this->order,
         ];
     }
